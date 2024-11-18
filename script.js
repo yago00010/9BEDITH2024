273afc50d31fd4466d28d8c8ec522fcf2fca0f5d
yago00010 <yago.mota.silva@escola.pr.gov.br>
2024-11-18 10:14:28 -0400

    aula 2

---
+++ script.js
@@ -0,0 +1,8 @@
+function setup() {
+     createCanvas {400, 400} ;
+     let palavra = prompt("Qual palvra deseja  codificas?");
+    
+     background("#01080");
+     console.log(palavra);
+
+     noloop();
